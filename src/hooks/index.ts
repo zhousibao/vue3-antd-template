198d9e740072a13ece53fcc6dd04b40e5ff760fa
zhoupan <zhoupan@shizhuang-inc.com>
2021-04-13 16:43:54 +0800

    修改

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,6 @@
 import { useAsync } from "./useAsync"
 import { usePages } from "./usePages"
-import { useEventBus } from './useEventBus'
+import { useEventBus } from './useEven'
 import { useOnline } from './useOnline'
 import { useTime } from './useTime'
 import { useFocus } from './useFocus'
